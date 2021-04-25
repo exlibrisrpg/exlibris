c75202a875b12967fe4949ef9819a784baa9a0dd
John Bannister <johnb@hey.com>
2021-04-25 14:25:37 +0100

    Remove unused actions in TagsController

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,49 +3,7 @@ class TagsController < ApplicationController
     @tags = Tag.all
   end
 
-  def new
-    @tag = Tag.new
-  end
-
-  def create
-    @tag = Tag.new(tag_params)
-
-    if @tag.save
-      redirect_to @tag
-    else
-      render :new
-    end
-  end
-
   def show
     @tag = Tag.with_includes.find(params[:id])
-  end
-
-  def edit
-    @tag = Tag.find(params[:id])
-  end
-
-  def update
-    @tag = Tag.find(params[:id])
-
-    if @tag.update(tag_params)
-      redirect_to @tag
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @tag = Tag.find(params[:id])
-
-    @tag.destroy
-
-    redirect_to tags_path
-  end
-
-  private
-
-  def tag_params
-    params.require(:tag).permit(:name)
   end
 end
