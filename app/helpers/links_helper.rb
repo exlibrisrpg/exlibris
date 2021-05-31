bd6bb2a4daec219aacbfe4f60e210fc801fe61b3
Matt McGlincy <mattmcglincy@gmail.com>
2021-05-31 13:19:41 -0500

    Show site-specific link icons. (#67)
    * Show site-specific link icons.
    
    * Remove redundant return.
    
    * Fight with linter.
    
    * Still fighting.
    
    * Fix end indent.
    
    * Now onto scss lint.
    
    * Address review comments.
    
    * Clean up link-icon.
    
    * Recreate as an actual svg.
    
    * Set primary class on site icons. Remove now-unnecessary scss file, and instead tweak valign and right-space in compositions/i_icon.
    
    * Standardise svg icons
    
    All tidied by exporting them from Figma. Strips out unnecessary SVG
    content, sets all fills to `black` and sets size to 24px. Removes
    classes from `icon-search.svg` and all icons now only support a single
    colour.
    
    * Tidy icon styling
    
    Now that the `.with-icon` composition is not needed in the admin section
    we can standardise it around adding icons to links and simplify a lot of
    modifying classes.
    
    * Combine link regexes with name and icon
    
    Gives us a single place to add new icons and naming patterns for
    particular commonly used links.
    
    Co-authored-by: John Bannister <johnb@hey.com>

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,16 +1,32 @@
 module LinksHelper
   LINK_EXPRESSIONS = [
-    /(?<name>itch\.io)/,
-    /^(?:https?:\/\/)?(?:www\.)?(?<name>[\w\d.]+)\/?/
+    {name: :match, icon: :itch, regex: /(?<name>itch\.io)/},
+    {name: "Dropbox", icon: :dropbox, regex: /^(?:https?:\/\/)?(?:www\.)?dropbox.com\/?/},
+    {name: "Facebook", icon: :facebook, regex: /^(?:https?:\/\/)?(?:www\.)?facebook.com\/?/},
+    {name: "Google", icon: :google, regex: /^(?:https?:\/\/)?(?:www\.)?google.com\/?/},
+    {name: "Mörk Borg", icon: :morkborg, regex: /^(?:https?:\/\/)?(?:www\.)?morkborg.com\/?/},
+    {name: :match, icon: :link, regex: /^(?:https?:\/\/)?(?:www\.)?(?<name>[\w\d.]+)\/?/}
   ]
 
   def link_name(link)
-    match = LINK_EXPRESSIONS.detect do |expression|
-      link.address =~ expression
+    expression = link_expression(link)
+
+    if expression[:name] == :match
+      return expression[:regex].match(link.address)["name"]
     end
 
-    return link.address unless match
+    expression[:name]
+  end
 
-    match.match(link.address)["name"]
+  def link_icon(link)
+    link_expression(link)[:icon]
+  end
+
+  private
+
+  def link_expression(link)
+    LINK_EXPRESSIONS.detect do |expression|
+      link.address =~ expression[:regex]
+    end
   end
 end
