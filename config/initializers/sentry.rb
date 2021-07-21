111b8765a1e544e284c042004722ac85a8563c7f
John Bannister <johnb@hey.com>
2021-07-21 09:41:42 +0100

    Set Sentry environment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,6 @@
-Sentry.init do |config|
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+if Rails.env.production?
+  Sentry.init do |config|
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+    config.environment = ENV.fetch("SENTRY_ENVIRONMENT")
+  end
 end
