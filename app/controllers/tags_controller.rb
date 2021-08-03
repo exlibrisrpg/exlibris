965203241d5f0f96251ca4cd879285110415d436
John Bannister <johnb@hey.com>
2021-08-03 07:24:58 +0100

    Reduce n+1 queries on tags#show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,8 +4,14 @@ class TagsController < ApplicationController
   end
 
   def show
-    @tag = Tag.with_includes.find(params[:id])
-    set_page_and_extract_portion_from @tag.entries.with_includes
+    @tag = Tag.includes(:rich_text_description).find(params[:id])
+    set_page_and_extract_portion_from @tag.entries
+    @entries = @page.records.strict_loading.includes(
+      :links,
+      :rich_text_description,
+      cover_attachment: :blob,
+      tags: [:tag_category, :rich_text_description]
+    )
 
     if request.path != tag_path(@tag)
       redirect_to @tag, status: :moved_permanently
