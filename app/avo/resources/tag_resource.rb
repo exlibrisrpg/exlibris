88a42645b2c19104a93d85a541613b64a30e26bb
John Bannister <johnb@hey.com>
2021-08-28 12:14:59 +0100

    Sort tags in admin tags index

--- app/avo/resources/tag_resource.rb
+++ app/avo/resources/tag_resource.rb
@@ -4,6 +4,9 @@ class TagResource < Avo::BaseResource
   self.search_query = ->(params:) do
     scope.containing(params[:q])
   end
+  self.resolve_query_scope = ->(model_class:) do
+    model_class.by_name
+  end
 
   field :name, as: :text, required: true, sortable: true, link_to_resource: true
   field :short_description, as: :text, visible: ->(resource:) { resource.model.tag_category&.short_description_required }, required: ->(resource:) { resource.model.tag_category&.short_description_required }
