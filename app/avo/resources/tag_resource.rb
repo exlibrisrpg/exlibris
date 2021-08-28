3f4b8e4eb22193185d506ba30127860c12ebdd83
John Bannister <johnb@hey.com>
2021-08-28 12:15:12 +0100

    Hide tag short description in admin index

--- app/avo/resources/tag_resource.rb
+++ app/avo/resources/tag_resource.rb
@@ -9,7 +9,7 @@ class TagResource < Avo::BaseResource
   end
 
   field :name, as: :text, required: true, sortable: true, link_to_resource: true
-  field :short_description, as: :text, visible: ->(resource:) { resource.model.tag_category&.short_description_required }, required: ->(resource:) { resource.model.tag_category&.short_description_required }
+  field :short_description, as: :text, visible: ->(resource:) { resource.model.tag_category&.short_description_required }, required: ->(resource:) { resource.model.tag_category&.short_description_required }, hide_on: :index
   field :order, as: :number, sortable: true
 
   field :description, as: :trix, always_show: true, attachments_disabled: true
