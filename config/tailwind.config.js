df78dc9819feb5d7208c81e8ec7e999803c1b287
John Bannister <johnb@hey.com>
2022-03-24 18:05:47 +0000

    Setup theme colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,5 +1,22 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
+function withOpacityValue(variable) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `hsl(var(${variable}))`
+    }
+    return `hsl(var(${variable}) / ${opacityValue})`
+  }
+}
+
+function colorDefinition(type) {
+  return Object.fromEntries(
+    Array.from(Array(13).keys())
+      .slice(1)
+      .map((i) => [i, withOpacityValue(`--color--${type}-${i}`)])
+  )
+}
+
 module.exports = {
   content: [
     "./app/components/**/*.erb",
@@ -13,7 +30,10 @@ module.exports = {
       current: "current",
       transparent: "transparent",
       black: "black",
-      white: "white"
+      white: "white",
+      accent: colorDefinition("accent"),
+      primary: colorDefinition("primary"),
+      secondary: colorDefinition("secondary")
     },
     fontFamily: {
       brand: "var(--font-family--brand)",
