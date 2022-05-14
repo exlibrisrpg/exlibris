99b7aef8db70412461f645cd1ec3f028dff961b6
John Bannister <johnb@hey.com>
2022-05-14 18:24:51 +0100

    Collect system-specific analytics
    Closes #470

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Exlibris
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join("lib")
 
     # Dump schema as sql so we get language configurations
     config.active_record.schema_format = :sql
