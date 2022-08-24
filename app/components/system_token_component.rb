27a72ba3b222b6ffe5d4b7041a971348639bf8b7
John Bannister <johnb@hey.com>
2022-08-24 09:46:20 +0100

    Remove responsive classes for site#show template and components

--- app/components/system_token_component.rb
+++ app/components/system_token_component.rb
@@ -16,8 +16,6 @@ class SystemTokenComponent < ViewComponent::Base
     class_names(
       "tw-bg-white/20 tw-shadow tw-border-white tw-rounded-full tw-inline-flex tw-items-center tw-transition tw-duration-200",
       "tw-px-4 tw-py-2 tw-border-2 tw-gap-2",
-      "md:tw-px-6 md:tw-py-3 md:tw-border-3 md:tw-gap-3",
-      "lg:tw-px-8 lg:tw-py-4 lg:tw-border-4 lg:tw-gap-4",
       "hover:tw-bg-white/40 hover:tw-shadow-md": live,
       "tw-opacity-60": !live
     )
@@ -27,8 +25,6 @@ class SystemTokenComponent < ViewComponent::Base
     class_names(
       "tw-font-brand tw-text-white tw-tracking-tight tw-drop-shadow-sm tw-flex-shrink-0 tw-font-extrabold",
       "tw-text-2xl",
-      "md:tw-text-3xl",
-      "lg:tw-text-5xl"
     )
   end
 
@@ -36,8 +32,6 @@ class SystemTokenComponent < ViewComponent::Base
     class_names(
       "tw-font-brand tw-text-white tw-bg-white/30 tw-rounded-full tw-lining-nums tw-leading-none tw-drop-shadow-sm tw-flex-shrink-0 tw-font-extrabold",
       "tw-text-xl tw-px-2 tw-py-0.5",
-      "md:tw-text-2xl md:tw-px-3",
-      "lg:tw-text-4xl lg:tw-px-4 lg:tw-py-1"
     )
   end
 
