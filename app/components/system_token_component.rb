5da8b836f6e8d056ba279fa0cd10c84655efbb53
John Bannister <johnb@hey.com>
2022-08-28 13:41:37 +0100

    Fix system token hover blurring effect

--- app/components/system_token_component.rb
+++ app/components/system_token_component.rb
@@ -14,8 +14,8 @@ class SystemTokenComponent < ViewComponent::Base
 
   def token_class
     class_names(
-      "tw-bg-white/20 tw-shadow tw-border-white tw-rounded-full tw-inline-flex tw-items-center tw-transition tw-duration-200 tw-px-4 tw-py-2 tw-border-2 tw-gap-2 tw-select-none tw-relative tw-overflow-hidden tw-group",
-      "hover:tw-bg-white/40 hover:tw-shadow-md": live,
+      "tw-bg-white/20 tw-shadow tw-border-white tw-rounded-full tw-inline-flex tw-items-center tw-transition tw-px-4 tw-py-2 tw-border-2 tw-gap-2 tw-select-none tw-relative tw-overflow-hidden tw-group",
+      "hover:tw-bg-white/40 hover:tw-shadow-md focus:tw-bg-white/40 focus:tw-shadow-md": live,
       "tw-opacity-60 tw-cursor-default": !live
     )
   end
