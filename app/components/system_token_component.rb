a3322e078a229db9ea58395c656e5561b22dd7b0
John Bannister <johnb@hey.com>
2022-08-28 12:14:52 +0100

    Show 'in progress' on hover for not live systems

--- app/components/system_token_component.rb
+++ app/components/system_token_component.rb
@@ -14,7 +14,7 @@ class SystemTokenComponent < ViewComponent::Base
 
   def token_class
     class_names(
-      "tw-bg-white/20 tw-shadow tw-border-white tw-rounded-full tw-inline-flex tw-items-center tw-transition tw-duration-200 tw-px-4 tw-py-2 tw-border-2 tw-gap-2 tw-select-none",
+      "tw-bg-white/20 tw-shadow tw-border-white tw-rounded-full tw-inline-flex tw-items-center tw-transition tw-duration-200 tw-px-4 tw-py-2 tw-border-2 tw-gap-2 tw-select-none tw-relative tw-overflow-hidden tw-group",
       "hover:tw-bg-white/40 hover:tw-shadow-md": live,
       "tw-opacity-60 tw-cursor-default": !live
     )
