abe7257691cce710817f96eeddc57d2f810c02eb
mcglincy <mattmcglincy@gmail.com>
2022-10-30 07:45:01 -0500

    Add deathinspace to systems.

--- config/initializers/fixture_helpers.rb
+++ config/initializers/fixture_helpers.rb
@@ -14,7 +14,7 @@ if !Rails.env.production?
 
     # The collection of systems we want to generate seeds for
     def systems
-      %w[mausritter mork_borg]
+      %w[death_in_space mausritter mork_borg]
     end
 
     # The standard set of categories we should define on each system
